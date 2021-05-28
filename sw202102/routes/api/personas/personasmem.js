2ba7e4f1c2d812463d74a67c21c52a31499560f6
Orlando J Betancourth <orlando.betancourth@gmail.com>
2021-05-27 18:55:29 -0600

    Saving to File using Node.js FS

--- sw202102/routes/api/personas/personasmem.js
+++ sw202102/routes/api/personas/personasmem.js
@@ -1,6 +1,43 @@
 //Modelo de Datos en Memory
+// Guardando serializando la informacion en un documento json
+
+
+// serializar
+const fs = require('fs');
+
+const filename = "persona.json";
+
+let fileFirstRead = false;
+
+const writeToFile = (handler)=>{
+  fs.writeFile(
+    filename, // archivo
+    JSON.stringify(personasList), //informacion JSON
+    handler //la funcion que se disapara un vez guardado el archivo
+  );
+}
+
+const readFile = (handler)=>{
+  fs.readFile(
+    filename,
+    'utf8',
+    handler
+  );
+}
 
 var personasList = [];
+
+if(!fileFirstRead){
+  readFile(
+    (err,object)=>{
+      if(err){
+        console.log(err);
+      }else{
+        personasList = JSON.parse(object);
+      }
+    }
+  );
+}
 
 let personaStruct = {
   nombre:"",
@@ -37,6 +74,15 @@ module.exports.getStruct = ()=>{
 
 module.exports.addToList = ( persona ) =>{
   personasList.push(persona);
+  writeToFile(
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("File Succesfully Saved!!!");
+      }
+    }
+  );
   return personasList.length -1;
 }
 
@@ -46,6 +92,15 @@ module.exports.update = (id, nombre, telefono, correo, bio) => {
     ...personasList[id], // old values
     ...{ nombre, telefono, correo, bio } // new values
   };
+  writeToFile(
+    (err)=>{
+      if(err){
+        console.log(err);
+      }else {
+        console.log("File Succesfully Saved!!!");
+      }
+    }
+  );
   return personasList[id];
   /*
   personasList[id] = Object.assign(
@@ -63,5 +118,14 @@ module.exports.deletePersona = (id)=>{
     }
   );
   personasList = newPersonasList;
+  writeToFile(
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("File Succesfully Saved!!!");
+      }
+    }
+  );
   return true;
 }
